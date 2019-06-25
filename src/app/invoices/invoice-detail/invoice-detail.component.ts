b037f8846eb5b5ccdf84b085ff38019462a2d26a
Joseph Mtinangi <josephmtinangi@gmail.com>
2019-06-25 16:48:12 +0300

    Show invoice

--- src/app/invoices/invoice-detail/invoice-detail.component.ts
+++ src/app/invoices/invoice-detail/invoice-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Invoice } from 'src/app/_models/invoice.model';
+import { ApiServiceService } from 'src/app/api-service.service';
+import { RealEstateAgent } from 'src/app/_models/real_estate_agent.model';
+import { Customer } from 'src/app/_models/customer.model';
 
 @Component({
   selector: 'app-invoice-detail',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoiceDetailComponent implements OnInit {
 
-  constructor() { }
+  invoice: Invoice;
+  agent: RealEstateAgent;
+  customer: Customer;
+  customerContract: any;
+  customerPaymentSchedule: any;
+
+  constructor(private route: ActivatedRoute, private apiService: ApiServiceService) { }
 
   ngOnInit() {
+    this.getInvoice();
+  }
+
+  getInvoice():void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.apiService.getInvoice(id).subscribe((res: any) => {
+      this.invoice = res.data.invoice;
+      this.agent = res.data.agent;
+      this.customerContract = res.data.customerContract;
+      this.customer = res.data.customerContract.customer;
+      this.customerPaymentSchedule = res.data.customerPaymentSchedule;
+    });
   }
 
 }
